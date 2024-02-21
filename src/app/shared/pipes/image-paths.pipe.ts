4e66bf6b9bdd4115d04a4e23bc62537bcd4b6098
anadiradze <anadi16@freeuni.edu.ge>
2024-02-21 20:42:07 +0400

    ref: Reorganize project directories and files for improved cleanliness

---
+++ src/app/shared/pipes/image-paths.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { imageByCategory } from '../../constants/images.enum';
+
+@Pipe({
+  name: 'imagePaths',
+  standalone: true,
+})
+export class ImagePathsPipe implements PipeTransform {
+  transform(value: string): any {
+    let imagepath;
+
+    switch (value) {
+      case imageByCategory.Star:
+        imagepath = 'assets/lists/star.svg';
+        break;
+      case imageByCategory.New:
+        imagepath = 'assets/lists/new.svg';
+        break;
+      case imageByCategory.Gift:
+        imagepath = 'assets/lists/buyBonus.svg';
+        break;
+    }
+    return imagepath;
+  }
+}
